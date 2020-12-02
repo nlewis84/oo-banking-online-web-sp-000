a6f7dade4e464481fcef924c0f65d9317b3e51ea
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-02 05:35:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid?
+    if self.valid? && self.status = "pending"
       sender.balance -= amount
       receiver.balance += amount 
       self.status = "complete"
