e6ecf48462324588ff9d4a710ef0d83d5e96a42c
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-02 05:28:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       self.status = "complete"
       self.amount = 0
     elsif !self.valid?
-      "Transaction rejected. Please check your account balance
+      "Transaction rejected. Please check your account balance"
       self.status = "rejected"
     end
   end
